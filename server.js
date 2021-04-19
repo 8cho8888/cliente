bb843fac11f131be3358e208629356b70d56790d
unknown <481600136@utzac.edu.mx>
2021-04-19 11:21:34 -0500

    Se actualizo el server.js

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const express = require('express');
 const app = express();
 app.use(requireHTTPS);
 
-app.use(express.static('./dist/client'));
+app.use(express.static('./dist'));
 
 app.get('/*', function (req, res) {
-    res.sendFile('index.html', { root: 'dist/client/' });
+    res.sendFile('index.html', { root: 'dist' });
 });
 
 app.listen(process.env.PORT || 8080);
